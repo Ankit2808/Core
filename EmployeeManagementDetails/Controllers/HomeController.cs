677716bfff1cdc1c667945e2e9ec1d870e2aa377
Your Name <you@example.com>
2020-03-05 23:36:51 +0530

    till part 20

---
+++ EmployeeManagementDetails/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagementDetails.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementDetails.Controllers
+{
+    public class HomeController:Controller
+    {
+        private IEmployeeRepositary _employeeRepository;
+
+        public HomeController(IEmployeeRepositary employeeRepositary)
+        {
+            _employeeRepository = employeeRepositary;
+        }
+
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+    }
+}
---
+++ EmployeeManagementDetails/Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementDetails.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepositary
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){ Id=1,Name="Ankit",Department="M.s" },
+                new Employee(){ Id=2,Name="Akash",Department="Mechanical" },
+                new Employee(){ Id=3,Name="Shalu",Department="Testing" }
+            };
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _employeeList.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
